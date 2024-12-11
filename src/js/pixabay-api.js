796ed530fe0fe4fa203a120a0179b9fc97945141
Olyamh <olyamh234@yahoo.com>
2024-12-11 12:31:46 +0200

    hw

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,50 @@
+import {createMarkup} from "./render-functions"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+export const gallery = document.querySelector(".gallery");
+export const loader = document.querySelector('.loader')
+const form = document.querySelector(".js-search-form");
+
+export function pixabayApi(url, options){
+   return fetch(`${url}?${options}`)
+   .then((response) => {
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+loader.style.visibility = 'visible'
+    return response.json();
+})
+.then(data => {
+    console.log("get:" , data);
+    setTimeout(() => {
+        
+        if (data.hits.length === 0){
+            iziToast.show({
+                messageColor: 'white',
+                backgroundColor: 'red',
+                position: 'topRight',
+                close: true,
+                title: '',
+                message: 'Sorry, there are no images matching your search query. Please try again!'
+            });
+        }
+        gallery.innerHTML += createMarkup(data.hits);
+        const lightbox = new SimpleLightbox('.gallery-item a', {
+            captionDelay: 250,
+            captions: true,
+            captionsData: "alt",
+        });
+        
+        lightbox.refresh();
+
+        loader.style.visibility = 'hidden'
+    }, 2000);
+})
+.catch(error => console.error(error))
+.finally(() => form.reset())
+
+}
+
---
+++ src/main.js
@@ -0,0 +1,41 @@
+
+import { pixabayApi } from "./js/pixabay-api";
+import { gallery } from "./js/pixabay-api";
+import { loader } from "./js/pixabay-api";
+
+loader.style.visibility = 'hidden';
+
+const API_KEY = "47534106-2225be5d16534437522f359a0";
+const params = new URLSearchParams({
+    key: API_KEY,
+    q: "",
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true
+});
+const BASE_URL = "https://pixabay.com/api/";
+
+const form = document.querySelector(".js-search-form");
+
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    gallery.innerHTML = "";
+
+         
+            const input = event.target.querySelector('.search-input');
+            const clientInput  = input.value.trim();
+           
+            console.log("input:", clientInput);
+            params.set('q', clientInput);
+            pixabayApi(BASE_URL, params);
+        
+});
+
+
+
+
+
+
+
+
